13e0d5195c3a66f5815f1fb2712954f3b53e9cdf
Matias Simonetto <m.simonetto@caen.it>
2023-09-05 13:01:30 +0000

    solve exercise-1.2

--- chapter-01/simple-mc-main-1.cpp
+++ chapter-01/simple-mc-main-1.cpp
@@ -6,8 +6,9 @@
 using namespace std;
 
 double
-simple_montecarlo_1 (double expirity, double strike, double spot, double vol,
-                     double r, unsigned long number_of_paths)
+simple_montecarlo_1 (double expirity, double lower_strike, double upper_strike,
+                     double spot, double vol, double r,
+                     unsigned long number_of_paths)
 {
   double variance = vol * vol * expirity;
   double root_variance = sqrt (variance);
@@ -15,18 +16,17 @@ simple_montecarlo_1 (double expirity, double strike, double spot, double vol,
 
   double moved_spot = spot * exp (r * expirity + ito_correction);
   double this_spot;
-  double running_sum = 0;
+  unsigned long running_sum = 0;
 
   for (unsigned long i = 0; i < number_of_paths; i++)
     {
       double this_gaussian = get_one_gaussian_by_box_muller ();
       this_spot = moved_spot * exp (root_variance * this_gaussian);
-      double this_payoff = this_spot - strike;
-      this_payoff = this_payoff > 0 ? this_payoff : 0;
-      running_sum += this_payoff;
+      if (this_spot > lower_strike && this_spot < upper_strike)
+        running_sum++;
     }
 
-  double mean = running_sum / number_of_paths;
+  double mean = static_cast<double> (running_sum) / number_of_paths;
   mean *= exp (-r * expirity);
   return mean;
 }
@@ -35,7 +35,8 @@ int
 main ()
 {
   double expirity;
-  double strike;
+  double lower_strike;
+  double upper_strike;
   double spot;
   double vol;
   double r;
@@ -44,8 +45,11 @@ main ()
   cout << "Enter expirity" << endl;
   cin >> expirity;
 
-  cout << "Enter strike" << endl;
-  cin >> strike;
+  cout << "Enter lower strike" << endl;
+  cin >> lower_strike;
+
+  cout << "Enter upper strike" << endl;
+  cin >> upper_strike;
 
   cout << "Enter spot" << endl;
   cin >> spot;
@@ -59,8 +63,8 @@ main ()
   cout << "Enter number of paths" << endl;
   cin >> number_of_paths;
 
-  double result
-      = simple_montecarlo_1 (expirity, strike, spot, vol, r, number_of_paths);
+  double result = simple_montecarlo_1 (expirity, lower_strike, upper_strike,
+                                       spot, vol, r, number_of_paths);
 
   cout << "The price is " << result << endl;
 }
